32ed3d132e709f58ac50c77d430355fb5e910bdd
Phéna Proxima <adam@phenaproxima.net>
2021-02-04 16:06:53 -0500

    Decompose the activate method into smaller ones

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,6 +7,7 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Repository\ComposerRepository;
 use Composer\Repository\RepositoryFactory;
+use Composer\Repository\RepositoryManager;
 use Tuf\ComposerIntegration\Repository\TufValidatedComposerRepository;
 
 class Plugin implements PluginInterface
@@ -16,31 +17,37 @@ class Plugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $loop = $composer->getLoop();
-        $httpDownloader = $loop->getHttpDownloader();
-        $dispatcher = $composer->getEventDispatcher();
-        $config = $composer->getConfig();
-
         // By the time this plugin is activated, several repositories may have
         // already been instantiated, and we need to convert them to
         // TUF-validated repositories. Unfortunately, the repository manager
         // only allows us to add new repositories, not replace existing ones.
         // So we have to rebuild the repository manager from the ground up to
         // add TUF validation to the existing repositories.
-        $oldManager = $composer->getRepositoryManager();
-        $newManager = RepositoryFactory::manager($io, $config, $loop->getHttpDownloader(), $dispatcher, $loop->getProcessExecutor());
-        $newManager->setLocalRepository($oldManager->getLocalRepository());
-        // Ensure that any repositories added later will be validated by TUF if
-        // configured accordingly.
+        $newManager = $this->createNewRepositoryManager($composer, $io);
+        $this->addTufValidationToRepositories($composer, $newManager, $io);
+        $composer->setRepositoryManager($newManager);
+    }
+
+    private function createNewRepositoryManager(Composer $composer, IOInterface $io): RepositoryManager
+    {
+        $loop = $composer->getLoop();
+        $newManager = RepositoryFactory::manager($io, $composer->getConfig(), $loop->getHttpDownloader(), $composer->getEventDispatcher(), $loop->getProcessExecutor());
+        $newManager->setLocalRepository($composer->getRepositoryManager()->getLocalRepository());
+        // Ensure that any Composer repositories added to this manager will be
+        // validated by TUF if configured accordingly.
         $newManager->setRepositoryClass('composer', TufValidatedComposerRepository::class);
 
-        foreach ($oldManager->getRepositories() as $repository) {
+        return $newManager;
+    }
+
+    private function addTufValidationToRepositories(Composer $composer, RepositoryManager $manager, IOInterface $io): void
+    {
+        foreach ($composer->getRepositoryManager()->getRepositories() as $repository) {
             if ($repository instanceof ComposerRepository) {
-                $repository = new TufValidatedComposerRepository($repository->getRepoConfig(), $io, $config, $httpDownloader, $dispatcher);
+                $repository = new TufValidatedComposerRepository($repository->getRepoConfig(), $io, $composer->getConfig(), $composer->getLoop()->getHttpDownloader(), $composer->getEventDispatcher());
             }
-            $newManager->addRepository($repository);
+            $manager->addRepository($repository);
         }
-        $composer->setRepositoryManager($newManager);
     }
 
     /**
