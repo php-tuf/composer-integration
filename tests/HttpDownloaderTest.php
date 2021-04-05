bf304a28a0851f24ba93a38db4114b1f40d86532
Phéna Proxima <adam@phenaproxima.net>
2021-04-05 10:19:16 -0400

    Add more test cases

--- tests/HttpDownloaderTest.php
+++ tests/HttpDownloaderTest.php
@@ -5,6 +5,7 @@ namespace Tuf\ComposerIntegration\Tests;
 use Composer\Util\Filesystem;
 use Composer\Util\HttpDownloader;
 use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Promise\RejectedPromise;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Utils;
 use PHPUnit\Framework\TestCase;
@@ -13,16 +14,33 @@ use Tuf\Client\ResponseStream;
 use Tuf\Client\Updater;
 use Tuf\ComposerIntegration\HttpDownloaderAdapter;
 use Tuf\ComposerIntegration\Repository\TufValidatedComposerRepository;
+use Tuf\Exception\PotentialAttackException\InvalidHashException;
+use Tuf\Exception\RepoFileNotFound;
 
 /**
  * @coversDefaultClass \Tuf\ComposerIntegration\HttpDownloaderAdapter
  */
 class HttpDownloaderTest extends TestCase
 {
+    /**
+     * The TUF-aware HTTP downloader under test.
+     *
+     * @var \Tuf\ComposerIntegration\HttpDownloaderAdapter
+     */
     private $downloader;
 
+    /**
+     * A mocked TUF-validated Composer repository.
+     *
+     * @var \Tuf\ComposerIntegration\Repository\TufValidatedComposerRepository
+     */
     private $repository;
 
+    /**
+     * Directory where TUF data is persisted for the test repository.
+     *
+     * @var string
+     */
     private $storageDir;
 
     /**
@@ -61,22 +79,47 @@ class HttpDownloaderTest extends TestCase
      */
     public function testGet(): void
     {
+        $responses = [];
+        // A boring, successful response.
         $response = new Response(200, [], Utils::streamFor('My bag of evil tricks has no limit!'));
+        $responses[] = new FulfilledPromise(new ResponseStream($response));
+        // A 304 (not modified) response with no body, which would normally
+        // trigger a TUF exception.
+        $error = new InvalidHashException(new ResponseStream(new Response(304)), '', 304);
+        $responses[] = new RejectedPromise($error);
+        // A RepoFileNotFound (i.e., a 404), which should result in an exception.
+        $responses[] = new RejectedPromise(new RepoFileNotFound('Ask moar nicely.'));
+
         $updater = $this->prophesize(Updater::class);
         $updater->download('packages.json', Argument::cetera())
-            ->willReturn(new FulfilledPromise(new ResponseStream($response)))
+            ->willReturn(...$responses)
             ->shouldBeCalled();
 
         $this->downloader->addRepository($this->repository, $updater->reveal());
 
-        $response = $this->downloader->get('https://test.net/packages.json', [
+        $url = 'https://test.net/packages.json';
+        $options = [
           'tuf' => [
             'repository' => 'https://test.net',
           ],
-        ]);
+        ];
+        $response = $this->downloader->get($url, $options);
         // The response should be converted to a native Composer response.
         $this->assertInstanceOf('\Composer\Util\Http\Response', $response);
         $this->assertSame('My bag of evil tricks has no limit!', $response->getBody());
+
+        $response = $this->downloader->get($url, $options);
+        // This 304 response would normally be an error, but should be converted
+        // to a native Composer response with an empty body.
+        $this->assertInstanceOf('\Composer\Util\Http\Response', $response);
+        $this->assertSame(304, $response->getStatusCode());
+        $this->assertEmpty($response->getBody());
+
+        // A RepoFileNotFound exception should be converted to a native Composer
+        // TransportException.
+        $this->expectException('\Composer\Downloader\TransportException');
+        $this->expectExceptionMessage('Ask moar nicely.');
+        $this->downloader->get($url, $options);
     }
 
     /**
