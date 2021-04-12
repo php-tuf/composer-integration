c8e39e1c18abaad575ec9357d352ee0e8c757fed
Phéna Proxima <adam@phenaproxima.net>
2021-04-12 13:07:29 -0400

    Remove scary things

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,17 +4,14 @@ namespace Tuf\ComposerIntegration;
 
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\Factory;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PreFileDownloadEvent;
+use Composer\Plugin\PostFileDownloadEvent;
 use Composer\Repository\ComposerRepository;
 use Composer\Repository\RepositoryFactory;
 use Composer\Repository\RepositoryManager;
 use Composer\Util\Filesystem;
-use Composer\Util\HttpDownloader;
-use Composer\Util\Loop;
 use Tuf\ComposerIntegration\Repository\TufValidatedComposerRepository;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
@@ -25,34 +22,16 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-          PluginEvents::PRE_FILE_DOWNLOAD => ['preFileDownload', -1000],
+            PluginEvents::POST_FILE_DOWNLOAD => ['postFileDownload', -1000],
         ];
     }
 
-    /**
-     * Reacts when a package is about to be downloaded.
-     *
-     * Note that this event handler is undebuggable due to some inexplicable
-     * bananapantsing in Composer's plugin manager. It copies the code of this
-     * class, renames it, and evals it into existence...which means it's not
-     * debuggable, since the code doesn't concretely exist in a place where
-     * Xdebug can find it. Dafuq! (See
-     * \Composer\Plugin\PluginManager::registerPackage() if you don't believe
-     * me.)
-     *
-     * @param \Composer\Plugin\PreFileDownloadEvent $event
-     *   The event object.
-     */
-    public function preFileDownload(PreFileDownloadEvent $event): void
+    public function postFileDownload(PostFileDownloadEvent $event): void
     {
-        if ($event->getType() === 'package') {
-            /** @var \Composer\Package\PackageInterface $package */
-            $package = $event->getContext();
-            // If the package is protected by TUF, its repository URL and target
-            // key should have been set by
-            // \Tuf\ComposerIntegration\PackageLoader::loadPackages().
-            if (array_key_exists('tuf', $package->getTransportOptions())) {
-                $event->getHttpDownloader()->setPackageUrl($package, $event->getProcessedUrl());
+        if ($event->getType() === 'metadata') {
+            $context = $event->getContext();
+            if ($context['repository'] instanceof TufValidatedComposerRepository) {
+                $context['repository']->validateMetadata($event->getUrl(), $context['response']);
             }
         }
     }
@@ -62,13 +41,6 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        // Finish any pending transfers, then swap out the HTTP downloader with
-        // a TUF-aware one.
-        $downloader = $composer->getLoop()->getHttpDownloader();
-        $downloader->wait();
-        $downloader = new HttpDownloaderAdapter($downloader, static::getStoragePath($composer));
-        $this->setHttpDownloader($composer, $io, $downloader);
-
         // By the time this plugin is activated, several repositories may have
         // already been instantiated, and we need to convert them to
         // TUF-validated repositories. Unfortunately, the repository manager
@@ -96,14 +68,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     {
         foreach ($composer->getRepositoryManager()->getRepositories() as $repository) {
             if ($repository instanceof ComposerRepository) {
-                $config = $repository->getRepoConfig();
-
-                if (isset($config['tuf'])) {
-                    $repository = new TufValidatedComposerRepository($config, $io, $composer->getConfig(), $composer->getLoop()->getHttpDownloader(), $composer->getEventDispatcher());
-                } else {
-                    // @todo Usability assessment. Should we output this for other repo types, or not at all?
-                    $io->warning("Authenticity of packages from ${config['url']} are not verified by TUF.");
-                }
+                $repository = new TufValidatedComposerRepository($repository->getRepoConfig(), $io, $composer->getConfig(), $composer->getLoop()->getHttpDownloader(), $composer->getEventDispatcher());
             }
             $manager->addRepository($repository);
         }
@@ -114,6 +79,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      */
     public function uninstall(Composer $composer, IOInterface $io)
     {
+        return;
+        // @todo Delete all persistent TUF data.
         $path = static::getStoragePath($composer);
         $io->info("Deleting TUF data in $path");
 
@@ -126,64 +93,5 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      */
     public function deactivate(Composer $composer, IOInterface $io)
     {
-        $downloader = $composer->getLoop()->getHttpDownloader();
-
-        if ($downloader instanceof HttpDownloaderAdapter) {
-            $this->setHttpDownloader($composer, $io, $downloader->getDecorated());
-        }
-    }
-
-    /**
-     * Swaps out the HTTP downloader.
-     *
-     * The HTTP downloader is a low-level service used by a lot of things.
-     * Therefore, we need to reinitialize the main event loop, the download
-     * manager, and the installation manager for the change to take full effect.
-     *
-     * @param \Composer\Composer $composer
-     *   The Composer instance.
-     * @param \Composer\IO\IOInterface $io
-     *   The I/O object.
-     * @param \Composer\Util\HttpDownloader $newDownloader
-     *   The new HTTP downloader to swap in.
-     */
-    private function setHttpDownloader(Composer $composer, IOInterface $io, HttpDownloader $newDownloader): void
-    {
-        $loop = new Loop($newDownloader, $composer->getLoop()->getProcessExecutor());
-        $composer->setLoop($loop);
-
-        $factory = new Factory();
-
-        $downloadManager = $factory->createDownloadManager($io, $composer->getConfig(), $newDownloader, $loop->getProcessExecutor(), $composer->getEventDispatcher());
-        $composer->setDownloadManager($downloadManager);
-
-        $installationManager = $factory->createInstallationManager($loop, $io, $composer->getEventDispatcher());
-        $composer->setInstallationManager($installationManager);
-
-        // It sucks to call a protected method, but if we don't do this, package
-        // installations and updates will fail hard. Hopefully we can fix this
-        // later if Composer makes Factory::createDefaultInstallers() public.
-        // @todo Support composer/installers and
-        // oomphinc/composer-installers-extender as well.
-        $reflector = new \ReflectionObject($factory);
-        $method = $reflector->getMethod('createDefaultInstallers');
-        $method->setAccessible(true);
-        $method->invoke($factory, $installationManager, $composer, $io, $loop->getProcessExecutor());
-    }
-
-    /**
-     * Gets the path where persistent TUF data should be stored.
-     *
-     * @param \Composer\Composer $composer
-     *   The Composer instance.
-     *
-     * @return string
-     *   The path where persistent TUF data should be stored.
-     */
-    private static function getStoragePath(Composer $composer): string
-    {
-        $vendorDir = $composer->getConfig()->get('vendor-dir');
-        $vendorDir = rtrim($vendorDir, DIRECTORY_SEPARATOR);
-        return implode(DIRECTORY_SEPARATOR, [$vendorDir, 'composer', 'tuf']);
     }
 }
--- src/Repository/TufValidatedComposerRepository.php
+++ src/Repository/TufValidatedComposerRepository.php
@@ -3,11 +3,18 @@
 namespace Tuf\ComposerIntegration\Repository;
 
 use Composer\Config;
+use Composer\Downloader\FilesystemException;
 use Composer\EventDispatcher\EventDispatcher;
 use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
 use Composer\Repository\ComposerRepository;
-use Tuf\ComposerIntegration\HttpDownloaderAdapter;
-use Tuf\ComposerIntegration\PackageLoader;
+use Composer\Util\Filesystem;
+use Composer\Util\Http\Response;
+use Composer\Util\HttpDownloader;
+use GuzzleHttp\Psr7\Utils;
+use Tuf\Client\DurableStorage\FileStorage;
+use Tuf\Client\GuzzleFileFetcher;
+use Tuf\Client\Updater;
 
 /**
  * Defines a Composer repository that is protected by TUF.
@@ -15,26 +22,74 @@ use Tuf\ComposerIntegration\PackageLoader;
 class TufValidatedComposerRepository extends ComposerRepository
 {
     /**
+     * @var Updater
+     */
+    private $updater;
+
+    /**
      * {@inheritDoc}
      */
-    public function __construct(array $repoConfig, IOInterface $io, Config $config, HttpDownloaderAdapter $httpDownloader, EventDispatcher $eventDispatcher = null)
+    public function __construct(array $repoConfig, IOInterface $io, Config $config, HttpDownloader $httpDownloader, EventDispatcher $eventDispatcher = null)
     {
-        // Ensure that all HTTP requests made by the parent class will identify
-        // which TUF repository they're associated with. The TUF-aware HTTP
-        // downloader keeps track of all instantiated TUF repositories and
-        // identifies them by their URL. We need to do this before calling the
-        // parent constructor because the options are stored in a private
-        // property.
-        // @see \Tuf\ComposerIntegration\HttpDownloaderAdapter::addRepository()
-        // @see \Tuf\ComposerIntegration\HttpDownloaderAdapter::createPromise()
-        $repoConfig['options']['tuf'] = [
-          'repository' => $repoConfig['url'],
-        ];
+        $url = $repoConfig['url'];
+
+        if (isset($repoConfig['tuf'])) {
+            // @todo: Write a custom implementation of FileStorage that stores repo keys to user's global composer cache?
+            // Use the repository URL to derive a path where we can persist the TUF
+            // data.
+            $repoPath = implode(DIRECTORY_SEPARATOR, [
+               rtrim($config->get('vendor-dir'), DIRECTORY_SEPARATOR),
+               'composer',
+               'tuf',
+                preg_replace('/[^[:alnum:]\.]/', '-', $url),
+            ]);
+
+            $fs = new Filesystem();
+            $fs->ensureDirectoryExists($repoPath);
+
+            // We expect the repository to have a root metadata file in a known
+            // good state. Copy that file to our persistent storage location if
+            // it doesn't already exist.
+            $rootFile = $repoPath . '/root.json';
+            if (!file_exists($rootFile)) {
+                $sourcePath = realpath($repoConfig['tuf']['root']);
+                if (!$fs->copy($sourcePath, $rootFile)) {
+                    throw new FilesystemException("Could not copy '$sourcePath' to '$rootFile");
+                }
+            }
+
+            $fetcher = GuzzleFileFetcher::createFromUri($url);
+            $this->updater = new Updater($fetcher, [], new FileStorage($repoPath));
+
+            $repoConfig['url'] .= '/targets';
+        } else {
+            // @todo Usability assessment. Should we output this for other repo types, or not at all?
+            $io->warning("Authenticity of packages from $url are not verified by TUF.");
+        }
         parent::__construct($repoConfig, $io, $config, $httpDownloader, $eventDispatcher);
-        // Make the HTTP downloader aware of this repository.
-        $httpDownloader->addRepository($this);
-        // The parent constructor sets up a package loader, so we need to
-        // override that with our TUF-aware one.
-        $this->loader = new PackageLoader($repoConfig['url'], $this->versionParser);
+    }
+
+    protected function configurePackageTransportOptions(PackageInterface $package)
+    {
+        parent::configurePackageTransportOptions($package);
+
+        $options = $package->getTransportOptions();
+        $config = $this->getRepoConfig();
+        $options['tuf'] = [
+            'repository' => $config['url'],
+            'target' => $package->getName() . '/' . $package->getVersion(),
+        ];
+        $package->setTransportOptions($options);
+    }
+
+    public function validateMetadata(string $url, Response $response): void
+    {
+        if ($this->updater) {
+            $config = $this->getRepoConfig();
+            $target = str_replace($config['url'], null, $url);
+            $target = ltrim($target, '/');
+            $stream = Utils::streamFor($response->getBody());
+            $this->updater->verify($target, $stream);
+        }
     }
 }
