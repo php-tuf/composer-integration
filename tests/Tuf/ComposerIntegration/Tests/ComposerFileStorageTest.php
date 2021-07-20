22e30c49e2cef4540f396db081ab4c5a5da6f90d
Phéna Proxima <adam@phenaproxima.net>
2021-07-20 14:14:39 -0400

    Add test coverage of ComposerFileStorage

---
+++ tests/Tuf/ComposerIntegration/Tests/ComposerFileStorageTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tuf\ComposerIntegration\Tests;
+
+use Composer\Config;
+use Composer\Util\Filesystem;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Tuf\ComposerIntegration\ComposerFileStorage;
+
+/**
+ * @coversDefaultClass \Tuf\ComposerIntegration\ComposerFileStorage
+ */
+class ComposerFileStorageTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private $vendorDir;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void
+    {
+        $this->vendorDir = sys_get_temp_dir() . '/vendor';
+        $this->assertTrue(putenv('COMPOSER_VENDOR_DIR=' . $this->vendorDir));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown(): void
+    {
+        (new Filesystem())->removeDirectoryPhp($this->vendorDir);
+    }
+
+    /**
+     * @covers ::basePath
+     */
+    public function testBasePath(): void
+    {
+        $config = new Config();
+        $this->assertSame("$this->vendorDir/composer/tuf", ComposerFileStorage::basePath($config));
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::create
+     */
+    public function testCreate(): void
+    {
+        $storage = TestComposerFileStorage::create('https://example.net/packages', new Config());
+        $expectedPath = "$this->vendorDir/composer/tuf/https---example.net-packages";
+        $this->assertSame($expectedPath, $storage->basePath);
+        $this->assertDirectoryExists($expectedPath);
+    }
+}
+
+class TestComposerFileStorage extends ComposerFileStorage
+{
+    public $basePath;
+}
