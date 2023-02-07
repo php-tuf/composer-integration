36fca2d1b58cbdd725444c8bc37eded35665c589
Phéna Proxima <adam@phenaproxima.net>
2023-02-07 14:55:31 -0500

    Make a change

--- src/TufValidatedComposerRepository.php
+++ src/TufValidatedComposerRepository.php
@@ -10,9 +10,11 @@ use Composer\Plugin\PreFileDownloadEvent;
 use Composer\Repository\ComposerRepository;
 use Composer\Util\Http\Response;
 use Composer\Util\HttpDownloader;
+use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Utils;
-use Tuf\Client\GuzzleFileFetcher;
 use Tuf\Exception\NotFoundException;
+use Tuf\Loader\GuzzleLoader;
+use Tuf\Loader\SizeCheckingLoader;
 use Tuf\Metadata\RootMetadata;
 use Tuf\Metadata\StorageInterface;
 
@@ -53,8 +55,13 @@ class TufValidatedComposerRepository extends ComposerRepository
         $url = rtrim($repoConfig['url'], '/');
 
         if (isset($repoConfig['tuf'])) {
+            $client = new Client([
+                // We need the trailing slash here in order for Guzzle to build
+                // the correct URLs.
+                'base_uri' => "$url/metadata/",
+            ]);
             $this->updater = new ComposerCompatibleUpdater(
-                GuzzleFileFetcher::createFromUri($url),
+                new SizeCheckingLoader(new GuzzleLoader($client)),
                 // @todo: Write a custom implementation of FileStorage that stores repo keys to user's global composer cache?
                 $this->initializeStorage($url, $config)
             );
