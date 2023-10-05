d3d6aafedc4589742b2bdf3a7ea46d6e8b9ae3d7
Phéna Proxima <adam@phenaproxima.net>
2023-10-05 14:14:12 -0400

    Fix LoaderTest

--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -2,15 +2,15 @@
 
 namespace Tuf\ComposerIntegration\Tests;
 
-use Composer\Downloader\MaxFileSizeExceededException;
-use Composer\Downloader\TransportException;
-use Composer\Util\HttpDownloader;
+use Composer\IO\NullIO;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\Http\Message\StreamInterface;
 use Tuf\ComposerIntegration\Loader;
-use Tuf\Exception\DownloadSizeException;
 use Tuf\Exception\RepoFileNotFound;
 
 /**
@@ -22,21 +22,19 @@ class LoaderTest extends TestCase
 
     public function testLoader(): void
     {
-        $downloader = $this->prophesize(HttpDownloader::class);
-        $loader = new Loader($downloader->reveal(), '/metadata/');
+        $responses = new MockHandler();
 
-        $downloader->get('/metadata/root.json', ['max_file_size' => 129])
-            ->willReturn(new Response())
-            ->shouldBeCalled();
+        $handlerStack = HandlerStack::create($responses);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $loader = new Loader($client, new NullIO());
+
+        $responses->append(new Response());
         $this->assertInstanceOf(StreamInterface::class, $loader->load('root.json', 128));
 
-        // Any TransportException with a 404 error could should be converted
+        // Any ClientException with a 404 error could should be converted
         // into a RepoFileNotFound exception.
-        $exception = new TransportException();
-        $exception->setStatusCode(404);
-        $downloader->get('/metadata/bogus.txt', ['max_file_size' => 11])
-            ->willThrow($exception)
-            ->shouldBeCalled();
+        $responses->append(new Response(404));
         try {
             $loader->load('bogus.txt', 10);
             $this->fail('Expected a RepoFileNotFound exception, but none was thrown.');
@@ -44,31 +42,15 @@ class LoaderTest extends TestCase
             $this->assertSame('bogus.txt not found', $e->getMessage());
         }
 
-        // A MaxFileSizeExceededException should be converted into a
-        // DownloadSizeException.
-        $downloader->get('/metadata/too_big.txt', ['max_file_size' => 11])
-            ->willThrow(new MaxFileSizeExceededException())
-            ->shouldBeCalled();
-        try {
-            $loader->load('too_big.txt', 10);
-            $this->fail('Expected a DownloadSizeException, but none was thrown.');
-        } catch (DownloadSizeException $e) {
-            $this->assertSame('too_big.txt exceeded 10 bytes', $e->getMessage());
-        }
-
-        // Any other TransportException should be wrapped in a
+        // Any other ClientException should be wrapped in a
         // \RuntimeException.
-        $originalException = new TransportException('Whiskey Tango Foxtrot', -32);
-        $downloader->get('/metadata/wtf.txt', ['max_file_size' => 11])
-            ->willThrow($originalException)
-            ->shouldBeCalled();
+        $responses->append(new Response(420));
         try {
             $loader->load('wtf.txt', 10);
             $this->fail('Expected a RuntimeException, but none was thrown.');
         } catch (\RuntimeException $e) {
-            $this->assertSame($originalException->getMessage(), $e->getMessage());
-            $this->assertSame($originalException->getCode(), $e->getCode());
-            $this->assertSame($originalException, $e->getPrevious());
+            $this->assertSame(420, $e->getCode());
+            $this->assertInstanceOf('Throwable', $e->getPrevious());
         }
     }
 }
