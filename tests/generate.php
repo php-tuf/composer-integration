882cbc582a553a63cd6b1ac45bd60d39af6ce647
Phéna Proxima <adam@phenaproxima.net>
2024-07-01 13:59:12 -0400

    Convert fixture generator to PHP

---
+++ tests/generate.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Composer\Autoload\ClassLoader;
+use Tuf\Tests\FixtureBuilder\Fixture;
+
+/** @var ClassLoader[] $class_loaders */
+$class_loaders = ClassLoader::getRegisteredLoaders();
+reset($class_loaders)
+    ->addPsr4('Tuf\\Tests\\', key($class_loaders) . '/php-tuf/php-tuf/tests');
+
+$fixture = new Fixture(__DIR__);
+$fixture->root->consistentSnapshot = true;
+
+// Create delegated roles and add their keys.
+$fixture->delegate('targets', 'package_metadata', [
+  'paths' => ['files/packages/8/p2/*'],
+]);
+$fixture->delegate('targets', 'package', [
+  'paths' => ['drupal/*'],
+]);
+
+// Add more targets here as needed.
+$dir = __DIR__ . '/_targets';
+$fixture->addTarget("$dir/packages.json");
+$fixture->targets['package_metadata']->add("$dir/token.json", 'files/packages/8/p2/drupal/token.json');
+$fixture->targets['package_metadata']->add("$dir/pathauto.json", 'files/packages/8/p2/drupal/pathauto.json');
+$fixture->targets['package']->add("$dir/token-1.9.0.zip", 'drupal/token/1.9.0.0');
+$fixture->targets['package']->add("$dir/pathauto-1.12.0.zip", 'drupal/pathauto/1.12.0.0');
+
+$fixture->publish();
