c1174e671d3152f417ed16af130847269e27b3c4
Adam <adam@phenaproxima.net>
2024-07-15 13:44:50 -0400

    Make the static cache persist for the entire life of the PHP process, to reduce HTTP requests for unmodified metadata (#120)

--- src/TufValidatedComposerRepository.php
+++ src/TufValidatedComposerRepository.php
@@ -67,7 +67,7 @@ class TufValidatedComposerRepository extends ComposerRepository
             // @todo: Write a custom implementation of FileStorage that stores repo keys to user's global composer cache?
             $storage = $this->initializeStorage($url, $config);
             $loader = new Loader($httpDownloader, $storage, $metadataUrl);
-            $loader = new StaticCache($loader, $io);
+            $loader = new StaticCache($loader, $io, $metadataUrl);
             $loader = new SizeCheckingLoader($loader);
             $this->updater = new ComposerCompatibleUpdater($loader, $storage);
 
